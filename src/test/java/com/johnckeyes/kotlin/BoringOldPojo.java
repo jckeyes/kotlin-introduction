6f2eb62422d1737ff9b96a00c396d8a9c16187e8
Fitzgerald, Andrew <andrew.fitzgerald@cardinalhealth.com>
2017-05-30 21:19:53 -0400

    give mr. pojo the boilerplate he deserves

--- src/test/java/com/johnckeyes/kotlin/BoringOldPojo.java
+++ src/test/java/com/johnckeyes/kotlin/BoringOldPojo.java
@@ -15,4 +15,26 @@ public class BoringOldPojo {
     public void setBlah(String blah) {
         this.blah = blah;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BoringOldPojo that = (BoringOldPojo) o;
+
+        return blah != null ? blah.equals(that.blah) : that.blah == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return blah != null ? blah.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BoringOldPojo{" +
+                "blah='" + blah + '\'' +
+                '}';
+    }
 }
