1114714cbae9a9959558200c8286cf468aae0ffc
John Keyes <john.keyes@cardinalhealth.com>
2017-06-27 12:37:12 -0400

    Making room for new files

--- src/test/kotlin/com/johnckeyes/kotlin/06_Collections.kt
+++ src/test/kotlin/com/johnckeyes/kotlin/06_Collections.kt
@@ -5,6 +5,11 @@ import org.junit.Test
 
 class Collections {
 
+    /**
+     * Collections come in two flavors: Mutable and Immutable
+     *
+     * Immutable collections can not be modified in any way.
+     */
     @Test
     fun `immutable list`() {
         val list: List<Int> = listOf(1, 2, 3)
@@ -12,14 +17,29 @@ class Collections {
         assertThat(list).hasSize(3)
     }
 
+
+
+    /**
+     * Mutable collections, on the other hand, can be modified in much the same
+     * way Java collections can.
+     */
     @Test
     fun `mutable list`() {
         val list: MutableList<Int> = mutableListOf(1, 2, 3)
         list.add(4)
+        list.add(5)
+        list.removeAt(0)
 
         assertThat(list).hasSize(4)
     }
 
+
+
+    /**
+     * Collections have a number of "modern" utility functions.
+     *
+     * Here is an example of a Map/Reduce
+     */
     @Test
     fun `map reduce`() {
         val list = listOf("a", "big", "fish")
@@ -28,6 +48,11 @@ class Collections {
         assertThat(sentence).isEqualTo("a big fish")
     }
 
+
+
+    /**
+     * Sets and Maps both have mutable and immutable implementations.
+     */
     @Test
     fun `other collections`() {
         val set = setOf(1, 2, 3)
